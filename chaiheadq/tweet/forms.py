8c658573127d7d8f0195475495994ae82b652dde
Talibiq123 <talibsaeed90@gmail.com>
2024-07-08 20:24:00 +0530

    deletion

--- chaiheadq/tweet/forms.py
+++ chaiheadq/tweet/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from models import Tweet
+from .models import Tweet
 
 class TweetForm(forms.ModelForm):
     class Meta:
--- chaiheadq/tweet/views.py
+++ chaiheadq/tweet/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
-from .django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
 
 # Create your views here.
 def index(request):
